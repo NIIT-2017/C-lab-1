e4f09d23228ad153caa27b4fea37259da0fe3377
sergone <medov777@yandex.ru>
2019-02-27 21:57:57 +0300

    B9-3

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,9 +1,12 @@
-#include "task2.h"
 #include <stdio.h>
+#include "task2.h"
+
 int main()
 {
-	int hour = 0; int min = 0; int sec = 0;
-	printf("Please, enter the current  time: HH:MM:SS ");
-	scanf("%d:%d:%d", &hour, &min, &sec);
+	int hour = 0;
+	int min = 0;
+	printf("Enter your time 'hh:mm'");
+	scanf("%d:%d", &hour, &min);
 	printf(greet(hour, min));
+	return 0;
 }
--- src/task2.cpp
+++ src/task2.cpp
@@ -1,12 +1,13 @@
 const char* greet(int hour, int min)
 {
-	if (hour >= 00 && hour < 06 && min >= 00 && min < 60)
-		return ("Good night!");
-	if (hour >= 06 && hour < 11 && min >= 00 && min < 60)
-		return ("Good moning!");
-	if (hour >= 11 && hour < 18 && min >= 00 && min < 60)
-		return ("Good day!");
-	if (hour >= 18 && hour < 24 && min >= 00 && min < 60)
-		return ("Good evening!");
-	else return ("Uncorrect time!");
+	if ((hour >= 0 && hour < 6) && (min >= 0 && min < 60))
+		return "Good night!";
+	else if ((hour >= 6 && hour < 11) && (min >= 0 && min < 60))
+		return "Good morning!";
+	else if ((hour >= 11 && hour < 18) && (min >= 0 && min < 60))
+		return "Good day!";
+	else if ((hour >= 18 && hour <= 23) && (min >= 0 && min < 60))
+		return "Good evening!";
+	else
+		return "Uncorrect time!";
 }
