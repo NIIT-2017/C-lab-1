324956e558ba675fb54d2e7eba3ddb3fcb374569
dkrasnykh <krasnykh.daria@gmail.com>
2019-11-20 23:05:21 +0300

    lab1 done

---
+++ src/main3.cpp
@@ -0,0 +1,21 @@
+﻿ #include <stdio.h>
+#include "task3.h"
+
+int main()
+{
+
+	double angle = 0;
+	char type = 0;
+	char buf[256];
+	//scanf( "%lf", );
+	int result = scanf("%lf%c", &angle, &type);
+	if (result == 0 || type != 'R' && type != 'D')
+	{
+		printf("Uncorrect value!\n");
+		return 1;
+	}
+
+	printf(convert(buf, angle, type));
+	
+	return 0;
+}
---
+++ src/task4.cpp
@@ -0,0 +1,8 @@
+#include "task4.h"
+
+float convert(int feet, int inches)
+{
+	float result = 0;
+	result = (feet * 12 + inches) * 2.54;
+	return result;
+}
