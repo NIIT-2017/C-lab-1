21d24f1b6eb0e727de23995b451688597cc9a3bd
TwinSss <chulan4ikmoi91@yandex.ru>
2019-11-23 20:35:08 +0300

    done

--- src/task2.cpp
+++ src/task2.cpp
@@ -4,20 +4,20 @@ const char * greet(int hour, int min)
 {
     
 
-    if ((hour > 24) || (min > 60))   // оператор выбора временного промежутка при котором будет возвращение строковой константы в функцию int main()
+    if (hour >= 24 || hour < 0 || min >= 60 || min < 0)   // оператор выбора временного промежутка при котором будет возвращение строковой константы в функцию int main()
         return "Uncorrect time!";
 
-    else if ((hour > 00) && (hour < 06))
-        return "Good morning!";
-
-    else if ((hour >= 06) && (hour < 11))
-        return "Good day!";
-
-    else if ((hour >= 11) && (hour < 18))
-        return "Good evening!";
-
-    else if ((hour >= 18) && (hour < 24))
+    else if (hour < 6)
         return "Good night!";
+
+    else if (hour < 11)
+        return "Good morning!";
+      
+    else if (hour < 18)
+        return "Good day!";
+      
+    else if (hour < 23)
+        return "Good evening!";
    
     
 
