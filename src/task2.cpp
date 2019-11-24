601e867f0d4234551b4e14a4b3dee3addc65fec0
Marina Postnikova <nerra@yandex.ru>
2019-11-24 14:05:16 +0300

    task2 remove \n from strings

--- src/task2.cpp
+++ src/task2.cpp
@@ -3,17 +3,17 @@
 const char* greet(int hour, int min) {
 
     if (hour < 0 || hour>24 || min < 0 || min >60) {
-        return "Uncorrect time!\n";
+        return "Uncorrect time!";
     }
     else if (hour >= 0 && hour < 6) {
-        return "Good night!\n";
+        return "Good night!";
     }
     else if (hour >= 6 && hour < 11) {
-        return "Good morning!\n";
+        return "Good morning!";
     }
     else if (hour >= 11 && hour < 18) {
-        return "Good day!\n";
+        return "Good day!";
     }
-    else return "Good evening!\n";
+    else return "Good evening!";
 }
 
