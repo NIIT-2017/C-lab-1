8f19a36341c03203ee20ffd68e59fd36895f8bb3
unknown <elik@example.com>
2019-12-05 18:14:19 +0300

    lab1 ready

---
+++ src/task2.cpp
@@ -0,0 +1,24 @@
+#include "task2.h"
+
+
+const char * greet(int hour, int min)
+{
+	    if (((hour < 00) && (hour > 24)) || ((min <= 00) && (min > 60)))
+			//проверяем правильность ввода, задаём ограничиващий диапазон ввода данных
+		return("Uncorrect time!\n");	
+
+		if ((hour >= 00 && hour <= 06)) 
+		return("Good night!\n"); 
+
+		else if ((hour >= 06 && hour <= 11))
+		return("Good morning!\n"); 
+
+	    else if ((hour >= 11 && hour <= 18))
+		return("Good day!\n"); 
+
+		else if ((hour >= 18 && hour < 24))
+		return("Good evening!\n"); 
+
+	return 0;
+}
+
