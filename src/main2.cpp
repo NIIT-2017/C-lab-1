66176b50d86fe80c54ff3332ffaeb1b20d49e8a5
beelt <anikipelov@gmail.com>
2019-12-06 22:09:26 +0300

    Выполнена задача №2

---
+++ src/main2.cpp
@@ -0,0 +1,16 @@
+#include "task2.h"
+#include <stdio.h>
+
+int main()
+{
+    int hour = 0, min = 0, sec = 0;
+    printf("Enter the current time in HH:MM:SS format:\n");
+    scanf("%d:%d:%d", &hour, &min, &sec);
+    
+    if (sec < 0 || sec > 59)
+        printf("Incorrect time!");
+    else
+        printf(greet(hour, min));
+
+    return 0;
+}
