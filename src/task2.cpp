2de927e35ee1b993b7195000efbea5bf9a9e3559
Ekaterina-Treushnikova <57718887+Ekaterina-Treushnikova@users.noreply.github.com>
2019-12-08 18:24:15 +0300

    Update task2.cpp
    Deleted - "\n''.

--- src/task2.cpp
+++ src/task2.cpp
@@ -2,13 +2,13 @@
 const char * greet(int hour, int min) //It's a data processing.
 {
 	if ((hour >= 0 && hour < 6) && (min >= 0 && min < 60))
-		return (char*)"Good night!\n";
+		return (char*)"Good night!";
 	else if ((hour >= 6 && hour < 11) && (min >= 0 && min < 60))
-		return (char*)"Good monight!\n";
+		return (char*)"Good monight!";
 	else if ((hour >= 11 && hour < 18) && (min >= 0 && min < 60))
-		return (char*)"Good day!\n";
+		return (char*)"Good day!";
 	else if ((hour >= 18 && hour < 24) && (min >= 0 && min < 60))
-		return (char*)"Good evening!\n";
+		return (char*)"Good evening!";
 	else
-		return (char*)"Uncorrect time!\n";
+		return (char*)"Uncorrect time!";
 }
