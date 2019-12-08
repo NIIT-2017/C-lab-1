198e9599255c8e48794e9db55c6839da57b8474e
Ekaterina-Treushnikova <57718887+Ekaterina-Treushnikova@users.noreply.github.com>
2019-12-08 19:01:16 +0300

    Update task2.cpp
    Correct "morning"

--- src/task2.cpp
+++ src/task2.cpp
@@ -4,7 +4,7 @@ const char * greet(int hour, int min) //It's a data processing.
 	if ((hour >= 0 && hour < 6) && (min >= 0 && min < 60))
 		return (char*)"Good night!";
 	else if ((hour >= 6 && hour < 11) && (min >= 0 && min < 60))
-		return (char*)"Good monight!";
+		return (char*)"Good morning!";
 	else if ((hour >= 11 && hour < 18) && (min >= 0 && min < 60))
 		return (char*)"Good day!";
 	else if ((hour >= 18 && hour < 24) && (min >= 0 && min < 60))
