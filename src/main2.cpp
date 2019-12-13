f4801db58adf26067755cbef7ab630e83b76da81
STI <sti152nnov@gmail.com>
2019-12-13 11:43:35 +0300

    ok

---
+++ src/main2.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task4.h"
+#include <stdio.h>
+
+void clean_stdin(void) // the option to clear the input  buffer
+{
+    int c;
+    do
+    {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+int main()
+{
+    int feet = 0, inches = 0;
+    printf("Hello, this program translates the British system of measures into metric.\n");
+#include "task2.h"
+#include <stdio.h>
+
+    void clean_stdin(void) // the option to clear the input buffer
+    {
+        int c;
+        do
+        {
+            c = getchar();
+        } while (c != '\n' && c != EOF);
+    }
+
+    int main()
+    {
+        int hour = 0, min = 0;
+        while (1)
+        {
+            printf("Please, enter time (hour:minute) : ");
+            if (scanf("%d:%d", &hour, &min) != 2)
+            {
+                printf("Incorrect answer, only numbers and positive! \n");
+                clean_stdin();
+                continue;
+            }
+            break;
+        }
+        printf("%s", greet(hour, min));
+        return 0;
+    }
