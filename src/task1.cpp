6cc4e414640694cca202f724c40f3c6786615700
SemKilyaev <57391254+SemKilyaev@users.noreply.github.com>
2020-01-19 16:44:15 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -4,27 +4,27 @@
 int getRecommendation(char gender, float height, float weight)
 {
     char p = gender;
-    char m_men = 'm';                        // ïåðåìåííàÿ ïðèñâàèâàåò çíà÷åíèå 'ì'
-    char w_men = 'w';                        //ïåðåìåííàÿ ïðèñâàèâàåò çíà÷åíèå 'w'
-    int optimal_weight = 0;                  // ïåðåìåííàÿ èäåàëüíîãî âåñà
+    char m_men = 'm';                       
+    char w_men = 'w';                        
+    int optimal_weight = 0;                 
     
 
-    if (p == m_men)                         //îïåðàòîð âûáîðà óñëîâèÿ ïåðåìåííîé char p = 'ì'
+    if (p == m_men)                         
     {
         optimal_weight = height - 100;
 
-        if (optimal_weight < weight)        //ñðàâíåíèå îïòèìàëüíîãî âåñà ñ ïîëó÷åííûìè äàííûìè îò ïîëüçîâàòåëÿ è âûâîä çíà÷åíèÿ (-1,0,1)
+        if (optimal_weight < weight)        
             return 1;
         else if (optimal_weight > weight)
             return -1;
         else if (optimal_weight == weight)
             return 0;
     }
-    else if (p == w_men)                    //îïåðàòîð âûáîðà óñëîâèÿ ïåðåìåííîé char p = 'w'
+    else if (p == w_men)                   
     {
         optimal_weight = height - 110;
 
-        if (optimal_weight < weight)      //ñðàâíåíèå îïòèìàëüíîãî âåñà ñ ïîëó÷åííûìè äàííûìè îò ïîëüçîâàòåëÿ è âûâîä çíà÷åíèÿ (-1,0,1)
+        if (optimal_weight < weight)     
             return 1;
         else if (optimal_weight > weight)
             return -1;
