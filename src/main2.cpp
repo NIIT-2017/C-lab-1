bf835453451b83e0d5e0664326ff262f5e0720be
Vasya Belov <rulit2009@yandex.ru>
2020-02-05 21:49:18 +0300

    change main2

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,9 +5,6 @@ int main()
     int hour = 0, min = 0, sec = 0;
     printf("Enter time (HH:MM:SS)\n");
     scanf("%d:%d:%d", & hour, & min, & sec);
-    if (sec < 0 || sec > 60)
-        printf("uncorrect time!\n");
-    else
         printf(greet(hour, min));
     return 0;
 }
