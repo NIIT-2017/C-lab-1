30f0caeda68f49e9c4630d9f8c8b9d4f51af1e1a
Eugeny Knyazev <black-1994@yandex.ru>
2020-02-05 21:35:30 +0300

    Clab1

--- src/task2.cpp
+++ src/task2.cpp
@@ -2,14 +2,14 @@ const char* greet(int hour, int min)
 {
 
 	if (hour >= 0 && hour < 6 && min >= 1 && min <= 59)
-		return ("Good night\n");
+		return ("Good night!\n");
 	else if (hour >= 6 && hour < 12 && min >= 1 && min <= 59)
-		return ("Good morning\n");
+		return ("Good morning!\n");
 	else if (hour >= 12 && hour < 18 && min >= 1 && min <= 59)
-		return ("Good day\n");
+		return ("Good day!\n");
 	else if (hour >= 18 && hour <= 23 && min >= 1 && min < 59)
-		return ("Good evening\n");
+		return ("Good evening!\n");
 	else
-		return ("Uncorrect time\n");
+		return ("Uncorrect time!\n");
 	
 }
