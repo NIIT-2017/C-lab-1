c674cdf4f4a0e5715f042a222da0f4bf2a9b0aff
Natalya Grigoreva <n-tixom@mail.ru>
2020-02-12 20:58:45 +0300

    lab1v3

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,13 +1,28 @@
-const char *greet(int hour, int min)
+const char* greet(int hour, int min)
 {
-	
-		if (hour>=0 && hour<6)
-			return "good night!\n";
-		else if (hour>=6 && hour<11)
-			return "good morning!\n";
-		else if (hour>=11 && hour<18)
-			return "good day!\n";
-		else //if (hour>=18 && hour<24)
-			return "good evening!\n";
-	
+	int time = 0;
+	if ((hour >= 0) && (hour < 6))
+		time = 1;
+	else
+		if ((hour >= 6) && (hour < 11))
+			time = 2;
+		else
+			if ((hour >= 11) && (hour < 18))
+				time = 3;
+			else 
+				if ((hour >= 18) && (hour < 24))
+					time = 4;
+	switch (time)
+	{
+		case 1:
+			return "Good night!";
+		case 2:
+			return "Good morning!";
+		case 3:
+			return "Good day!";
+		case 4:
+			return "Good evening!";
+		default:
+			return "Uncorrect time!";
+	}
 }
