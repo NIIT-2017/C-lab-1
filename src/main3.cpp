95768067e38182483505f15c55b6cf5559f65f6b
Alex Karpov <cool_07@mail.ru>
2020-02-27 17:34:33 +0300

    Lab-1 is ready

---
+++ src/main3.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task3.h"
+#include <stdio.h>
+#include <iostream>
+
+//#pragma warning(disaible:4996)
+
+int main()
+{
+	float angle = 0;
+	char type = '0';
+	printf("Enter angle in formate xx.xxD elso xx.xxR \n");
+	scanf("%f%c", &angle, &type);
+	char buf[100] = { 0 };
+	printf("%s\n", convert(buf, angle, type));
+	return 0;
+}
