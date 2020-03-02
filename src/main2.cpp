5cc52b9a3b2755f2cd7c5e5bc9d10069c30cc25c
VSpiridonova <57556209+VSpiridonova@users.noreply.github.com>
2020-03-03 00:45:55 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -4,11 +4,15 @@
 
 int main() 
     {
-        int hour = 0; 
-        int min = 0; 
-        int sec = 0; 
-            printf("What is time?\n"); 
-            scanf("%i:%i:%i", &hour, &min, &sec); 
-            printf("%s", greet(hour, min));
-        return 0;
+       int hour = 0; 
+        int min = 0;
+        int sec = 0;
+	        printf("Please, enter the time: HH:MM:SS ");
+	        scanf("%d:%d:%d", &hour, &min, &sec);
+	int hour = 0;
+	int min = 0;
+	printf("Enter your time 'hh:mm'");
+	scanf("%d:%d", &hour, &min);
+	printf(greet(hour, min));
+	return 0;
     }
