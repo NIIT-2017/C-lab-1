3e85dde1aaedc827a108e9b45d8b0e151d4ca449
VSpiridonova <57556209+VSpiridonova@users.noreply.github.com>
2020-03-02 23:31:41 +0300

    Add files via upload

---
+++ src/main4.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task4.h"
+#include <stdio.h>
+
+void clean_stdin(void)
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+int main() 
+{
+    int feet = 0;
+    int inches = 0;
+    while (1) {
+        printf("Write feet and inches in a right format 00'00 ");
+        int numberresult = scanf("%i'%i", &feet, &inches);
+        if (numberresult < 2) {
+            printf("You're wrong!\n");
+            clean_stdin();
+        }
+        else
+            break;
+    }
+    printf("%.1f", convert(feet, inches));
+    return 0;
+} 
