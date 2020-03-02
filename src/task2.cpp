1893914647f0ed94b269f9fc178cad327eb2dce0
VSpiridonova <57556209+VSpiridonova@users.noreply.github.com>
2020-03-03 00:43:14 +0300

    Update task2.cpp

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,15 +1,14 @@
 #include "task2.h"
 const char * greet(int hour, int min)
 {
-	    if (((hour < 00) && (hour > 24)) || ((min <= 00) && (min > 60)))
-		return("Uncorrect time!\n");	
-		if ((hour >= 00 && hour <= 06)) 
-		return("Good night!\n"); 
-		else if ((hour >= 06 && hour <= 11))
-		return("Good morning!\n"); 
-	    else if ((hour >= 11 && hour <= 18))
-		return("Good day!\n"); 
-		else if ((hour >= 18 && hour < 24))
-		return("Good evening!\n"); 
-	return 0;
+	    if ((hour >= 0 && hour < 6) && (min >= 0 && min < 60))
+		return "Good night!";
+	else if ((hour >= 6 && hour < 11) && (min >= 0 && min < 60))
+		return "Good morning!";
+	else if ((hour >= 11 && hour < 18) && (min >= 0 && min < 60))
+		return "Good day!";
+	else if ((hour >= 18 && hour <= 23) && (min >= 0 && min < 60))
+		return "Good evening!";
+	else
+		return "Uncorrect time!";
 }
