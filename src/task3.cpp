efe8c3979d45a07899aabd1c98e04c347f5027d3
VSpiridonova <57556209+VSpiridonova@users.noreply.github.com>
2020-03-03 00:08:38 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,18 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "task3.h"
-include <stdio.h>
-char* convert(char buf[], double angle, char type) {
-    double degree = 180;
-    double p = 3.14159;
-    double value = 0;
-    char  symbol = ' ';
-    if (type == 'D') {
-        value = (angle * p) / degree;
-        symbol = 'R';
-    } else if (type == 'R') {
-        value = (angle * degree) / p;
-        symbol = 'D';
-    }
-    sprintf(buf, "%lf%c", value, symbol);
-    return buf;
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <stdio.h>
+
+char* convert(char buf[], double angle, char type)
+{
+	double angle1 = 0; 
+	if (type == 'R')
+	{
+		angle1 = (((double)180 * angle) / M_PI);
+		sprintf(buf, "%3.2lf%c", angle1, 'D');
+	}
+	else
+	{
+		angle1 = (M_PI * angle) / (double)180;
+		sprintf(buf, "%3.2lf%c", angle1, 'R');
+	}
+	return buf;
 } 
