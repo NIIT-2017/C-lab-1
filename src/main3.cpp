422511ac08fc05ee14081b98327b46893f56ebb3
VSpiridonova <57556209+VSpiridonova@users.noreply.github.com>
2020-03-03 00:08:04 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,24 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "task3.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() 
+int main()
 {
-    char buf[256];
-    double angle = 0;
-    char type = ' ';
-
-    while (1) 
-	{
-        printf("You need enter value of angle in 'R' or 'D'.\n");
-        scanf("%lf%c", &angle, &type);
-        if (type != 'R' && type != 'D') {
-            printf("It's wrong!\n");
-        }
-        else 
+	char Buf[10] = { '0' };
+	char Type = ' ';
+	double Angle;// = 0;
+	printf("Enter the angle in degrees or radians: 00.00R vs 00.00D\n");
+	scanf("%lf %c", &Angle, &Type);
+	if ((Type != 'R') && (Type != 'D'))
 		{
-            printf(convert(buf, angle, type));
-            break;
-        }
-    return 0;
+			printf("Unknown value.\n");
+			return -1;
+		}
+	else
+		{
+			convert(Buf, Angle, Type);
+			printf("%3.2lf%c = %s\n", Angle, Type, Buf);
+		}
+	return 0;
 } 
